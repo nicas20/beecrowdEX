429ba3a56e532c793481dd5d93696edcbc3b86e9
nicas20 <100000527+nicas20@users.noreply.github.com>
2023-04-12 14:44:07 -0300

    1049 - Animal

--- Main.java
+++ Main.java
@@ -1,29 +1,40 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-
-    public static double calculoAjuste(double salario){
-        if (salario <= 400.00){
-            return salario * 1.15;
-        } else if (salario <= 800.00){
-            return salario * 1.12;
-        } else if (salario <= 1200.00){
-            return salario * 1.1;
-        } else if (salario <= 2000.00){
-            return salario * 1.07;
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String carac1 = sc.next();
+        String carac2 = sc.next();
+        String carac3 = sc.next();
+        if(carac1.equalsIgnoreCase("vertebrado")){
+            if(carac2.equalsIgnoreCase("ave")){
+                if(carac3.equalsIgnoreCase("carnivoro")){
+                    System.out.println("aguia");
+                } else {
+                    System.out.println("pomba");
+                }
+            } else {
+                if(carac3.equalsIgnoreCase("onivoro")){
+                    System.out.println("homem");
+                } else {
+                    System.out.println("vaca");
+                }
+            }
         } else {
-            return salario * 1.04;
+            if(carac2.equalsIgnoreCase("inseto")){
+                if(carac3.equalsIgnoreCase("hematofago")){
+                    System.out.println("pulga");
+                } else {
+                    System.out.println("lagarta");
+                }
+            } else {
+                if(carac3.equalsIgnoreCase("hematofago")){
+                    System.out.println("sanguessuga");
+                } else {
+                    System.out.println("minhoca");
+                }
+            }
         }
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        double salario = scanner.nextDouble();
-        double novoSalario = calculoAjuste(salario);
-        double reajusteGanho = novoSalario - salario;
-        double percentual = (reajusteGanho / salario) * 100;
-        System.out.printf("Novo salario: %.2f\n", novoSalario);
-        System.out.printf("Reajuste ganho: %.2f\n", reajusteGanho);
-        System.out.printf("Em percentual: %.0f %%\n", percentual);
     }
 }
